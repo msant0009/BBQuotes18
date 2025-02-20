d86132e472fd4dde5a75ac78f0336d4d6656fedb
Mark Santoro <mark_santoro@comcast.net>
2025-02-19 18:21:44 -0800

    Initial setup

---
+++ BBQuotes18/BBQuotes18App.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotes18App.swift
+//  BBQuotes18
+//
+//  Created by Mark Santoro on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotes18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBQuotes18/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  BBQuotes18
+//
+//  Created by Mark Santoro on 2/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView{
+            Tab("Breaking Bad", systemImage: "tortoise"){
+                Text("Breaking Bad View")
+                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
+            }
+            
+            Tab("Better Call Saul", systemImage: "briefcase"){
+                Text("Better Call Saul View")
+                    .toolbarBackgroundVisibility(.visible, for: .tabBar)
+            }
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
