edc29da30edf85eebb029e01c77f54a03b685a2b
Mark Santoro <mark_santoro@comcast.net>
2025-03-12 17:28:06 -0700

    finished fetch quote code

--- BBQuotes18/FetchService.swift
+++ BBQuotes18/FetchService.swift
@@ -22,18 +22,22 @@ struct FetchService {
         let quoteURL = baseURL.appending(path: "quotes/random")
         let fetchURL = quoteURL.appending(queryItems: [URLQueryItem(name: "production", value: show)])
             
-        
-        
         //Fetch Data
-        
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
         
         // handle response
-        
-        
+        // swift allows multiple guard checks on the same line:
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200// 200 = good response
+        else {
+            throw FetchError.badResponse
+        }
+              
         // decode data
+        let quote = try JSONDecoder().decode(Quote.self, from: data)
         
         //return quote
         
+        return quote
         
     }
     
