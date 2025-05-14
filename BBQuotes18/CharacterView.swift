446ccccab4266d469a501b10a4d1b464f0ebef6e
Mark Santoro <mark_santoro@comcast.net>
2025-05-14 09:55:45 -0700

    Add El Camino view. Improve character image view

--- BBQuotes18/CharacterView.swift
+++ BBQuotes18/CharacterView.swift
@@ -20,16 +20,22 @@ struct CharacterView: View {
                     .scaledToFit()
                 
                 ScrollView {
-                    AsyncImage(url: character.images[0]) { image in
-                        image
-                            .resizable()
-                            .scaledToFill()
-                    } placeholder: {
-                        ProgressView()
+                    TabView{
+                        ForEach(character.images, id: \.self) {characterImageURL in
+                            AsyncImage(url: characterImageURL) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                            } placeholder: {
+                                ProgressView()
+                        }
                     }
-                    .frame(width: geo.size.width/1.2, height: geo.size.height/1.7)
-                    .clipShape(.rect(cornerRadius: 25))
-                    .padding(.top, 60)
+                }
+                        .tabViewStyle(.page)
+                        .frame(width: geo.size.width/1.2, height: geo.size.height/1.7)
+                        .clipShape(.rect(cornerRadius: 25))
+                        .padding(.top, 60)
+                    
                     
                     VStack(alignment: .leading){
                         Text(character.name)
